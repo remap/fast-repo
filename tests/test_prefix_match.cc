0176112e50cef4ea7205ae74eed269fe2d379e00
peetonn <gpeetonn@gmail.com>
2018-10-20 22:53:05 -0700

    setup test harness

---
+++ tests/test_prefix_match.cc
@@ -0,0 +1,14 @@
+//
+// test_prefix_match.cc
+//
+//  Created by Peter Gusev on 20 October 2018.
+//  Copyright 2013-2018 Regents of the University of California
+//
+
+#include <stdlib.h>
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
